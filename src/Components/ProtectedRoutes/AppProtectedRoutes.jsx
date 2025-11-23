437fc4dfa28541a2675008d049559b9239034894
amira-mohamed99 <amira8813417@gmail.com>
2025-11-23 19:23:55 +0200

    fixes

--- src/Components/ProtectedRoutes/AppProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes/AppProtectedRoutes.jsx
@@ -2,8 +2,8 @@ import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../Context/UserContext.jsx";
 
-export default function AppProtectedRoutes({children}) {
-    const { token } = useContext(userContext);
+export default function AppProtectedRoutes({ children }) {
+  const { token } = useContext(userContext);
 
   const navigate = useNavigate();
 
@@ -13,8 +13,5 @@ export default function AppProtectedRoutes({children}) {
     }
   }, [token]);
 
- return (
-    <>
-      {children}
-    </>
-  )}
+  return <>{children}</>;
+}
